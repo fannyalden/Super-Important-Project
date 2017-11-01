c678249c7b528917302c07c618ded4a5aad1d679
Hanna Johansson <hanna93.hej@gmail.com>
2017-11-01 20:45:12 +0100

    correct data values added to chart

--- js/barchart.js
+++ js/barchart.js
@@ -4,7 +4,7 @@
 *************************************************************/
 
 //Create array of data to be visualized
-var dataArray = [23, 13, 21, 14, 37, 15, 18, 34, 30];
+var dataArray = [170000, 55000, 101700, 41500, 104300, 72400, 90000, 38000, 25000];
 
 //Create the SVG element. This is where we will place all of our shapes.
 //Then add attributes to the SVG.
@@ -18,12 +18,12 @@ svg.selectAll("rect")
     .enter().append("rect")		//Add a rectangle for each item in dataArray
 		.attr("class", "bar")	//Add a class name
 		//.attr("height","250")	//Height of the rectangles
-	    .attr("height", function(d, i) {return (d * 10)})
-	    .attr("width","40")		//Width of the rectangles
+	    .attr("height", function(d, i) {return (d * 0.002)})
+	    .attr("width","60")		//Width of the rectangles
 	    //.attr("x","25")			//Distance from the left side of the browser window
-	    .attr("x", function(d, i) {return i * 60 + 25})		//Add spacing between the rectangles, space = 60 pixels
+	    .attr("x", function(d, i) {return i * 65 + 25})		//Add spacing between the rectangles, space = 60 pixels
 	    //.attr("y","50");		//Distance from the top of the browser window
-	    .attr("y", function(d, i) {return 400 - (d * 10)});
+	    .attr("y", function(d, i) {return 600 - (d * 0.002)});
 
 //Add text to the bars
 svg.selectAll("text")
@@ -31,6 +31,6 @@ svg.selectAll("text")
     .enter().append("text")
     .text(function(d) {return d})
           .attr("class", "text")
-          .attr("x", function(d, i) {return (i * 60) + 36})
-          .attr("y", function(d, i) {return 420 - (d * 10)});
+          .attr("x", function(d, i) {return (i * 65) + 31})
+          .attr("y", function(d, i) {return 620 - (d * 0.002)});
 
