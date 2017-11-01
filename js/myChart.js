f6069161f9f2bbd1035411b26f6090c02b861745
Hanna Johansson <hanna93.hej@gmail.com>
2017-11-01 23:04:22 +0100

    added grouped bar chart

--- js/myChart.js
+++ js/myChart.js
@@ -1,32 +1,94 @@
+/************************************************************
+    EXAMPLE 1: Grouped bar chart
+*************************************************************/
+new Chart(document.getElementById("bar-chart-grouped"), {
+    type: 'bar',
+    data: {
+      labels: ["Cupertino", "Norrköping", "Frankfurt"],
+      datasets: [
+        {
+          label: "jTelefon",
+          backgroundColor: "rgba(255, 99, 132, 0.2)",
+          borderColor: "rgba(255, 99, 132, 1)",
+          borderWidth: 1,
+          data: [170000,55000,101700]
+        }, 
+        {
+          label: "jPlatta",
+          backgroundColor: "rgba(54, 162, 235, 0.2)",
+          borderColor: "rgba(54, 162, 235, 1)",
+          borderWidth: 1,
+          data: [41500,104300,72400]
+        },
+        {
+          label: "Päronklocka",
+          backgroundColor: "rgba(75, 192, 192, 0.2)",
+          borderColor: "rgba(75, 192, 192, 1)",
+          borderWidth: 1,
+          data: [90000,38000,25000]
+        }
+      ]
+    },
+    options: {
+    	legend: { display: true },
+	    title: {
+	        display: true,
+	        text: 'Lagersaldo'
+      	}
+    }
+});
 
+/************************************************************
+    EXAMPLE 2: Bar chart
+*************************************************************/ 
 var ctx = document.getElementById("myChart").getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        labels: ["jTelefon", "jPlatta", "Päronklocka", 
+        		 "jTelefon", "jPlatta", "Päronklocka", 
+        		 "jTelefon", "jPlatta", "Päronklocka"],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            /*label: 'Lagersaldo',*/
+            data: [170000, 415000, 90000, 55000, 104300, 38000, 101700, 72400, 25000],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(255, 99, 132, 0.2)',		//red
+                'rgba(54, 162, 235, 0.2)',		//blue
+                'rgba(75, 192, 192, 0.2)',		//green
+
+                'rgba(255, 99, 132, 0.2)',		//red
+                'rgba(54, 162, 235, 0.2)',		//blue
+                'rgba(75, 192, 192, 0.2)',		//green
+
+                'rgba(255, 99, 132, 0.2)',		//red
+                'rgba(54, 162, 235, 0.2)',		//blue
+                'rgba(75, 192, 192, 0.2)',		//green
+
+            /*
+                'rgba(255, 99, 132, 0.2)',		//red
+                'rgba(54, 162, 235, 0.2)',		//blue
+                'rgba(255, 206, 86, 0.2)',		//yellow
+                'rgba(75, 192, 192, 0.2)',		//green
+                'rgba(153, 102, 255, 0.2)',		//purple
+                'rgba(255, 159, 64, 0.2)'		//orange */
             ],
             borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(255,99,132,1)', 'rgba(54, 162, 235, 1)', 'rgba(75, 192, 192, 1)',
+                'rgba(255,99,132,1)', 'rgba(54, 162, 235, 1)', 'rgba(75, 192, 192, 1)',
+                'rgba(255,99,132,1)', 'rgba(54, 162, 235, 1)', 'rgba(75, 192, 192, 1)',
             ],
             borderWidth: 1
         }]
     },
     options: {
+      legend: { display: false },
+      title: {
+        display: true,
+        text: 'Lagersaldo'
+      }
+    }
+
+/*    options: {
         scales: {
             yAxes: [{
                 ticks: {
@@ -34,5 +96,5 @@ var myChart = new Chart(ctx, {
                 }
             }]
         }
-    }
+    }*/
 });
