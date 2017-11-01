160f4560b69de6a5c91209be972b34ee9ef546d8
Hanna Johansson <hanna93.hej@gmail.com>
2017-11-01 19:52:26 +0100

    added file for barchart

---
+++ js/barchart.js
@@ -0,0 +1,10 @@
+/************************************************************
+    Authors:  Fanny Aldén Hanna Johansson
+    Date: 2017-11-01
+*************************************************************/
+
+//Create array of data to be plotted
+var dataArray = [23, 13, 21, 14, 37, 15, 18, 34, 30];
+
+//Create the SVG element. This is where we will place all of our shapes. 
+var svg = d3.select("body").append("svg");
