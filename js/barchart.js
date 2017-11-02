d88e62b98a535852effef0eaa3728f06fded4794
Hanna Johansson <hanna93.hej@gmail.com>
2017-11-02 09:58:03 +0100

    removed what we are not using

--- js/barchart.js
+++ js/barchart.js
@@ -1,36 +1,65 @@
 /************************************************************
-    Authors:  Fanny Aldén Hanna Johansson
-    Date: 2017-11-01
+    Grouped bar chart
 *************************************************************/
+new Chart(document.getElementById("bar-chart-grouped"), {
+    type: 'bar',
+    data: {
+      labels: ["Cupertino", "Norrköping", "Frankfurt"],
+      datasets: [
+        {
+          label: "jTelefon",
+          backgroundColor: "rgba(255, 99, 132, 0.2)",
+          borderColor: "rgba(255, 99, 132, 1)",
+          borderWidth: 1,
+          data: [170000,55000,101700]
+        }, 
+        {
+          label: "jPlatta",
+          backgroundColor: "rgba(54, 162, 235, 0.2)",
+          borderColor: "rgba(54, 162, 235, 1)",
+          borderWidth: 1,
+          data: [41500,104300,72400]
+        },
+        {
+          label: "Päronklocka",
+          backgroundColor: "rgba(75, 192, 192, 0.2)",
+          borderColor: "rgba(75, 192, 192, 1)",
+          borderWidth: 1,
+          data: [90000,38000,25000]
+        }
+      ]
+    },
+    options: {
+    	legend: { display: true },
+	    title: {
+	        display: true,
+	        text: 'Lagersaldo'
+      	},
+        scales: {
+            yAxes: [{
+                display: false
+            }],
 
-//Create array of data to be visualized
-var dataArray = [170000, 55000, 101700, 41500, 104300, 72400, 90000, 38000, 25000];
+        }
+    }
+});
 
-//Create the SVG element. This is where we will place all of our shapes.
-//Then add attributes to the SVG.
-var svg = d3.select("body").append("svg")
-		.attr("height","100%")
-	    .attr("width","100%");
+//https://stackoverflow.com/questions/31631354/how-to-display-data-values-on-chart-js
+//Add plugin to display values of each bar
+Chart.pluginService.register({
+    afterDraw: function(chartInstance) {
+        var ctx = chartInstance.chart.ctx;
 
-//Add rectangles
-svg.selectAll("rect")
-    .data(dataArray)			//Tie this rectangle array to the data stored in dataArray	
-    .enter().append("rect")		//Add a rectangle for each item in dataArray
-		.attr("class", "bar")	//Add a class name
-		//.attr("height","250")	//Height of the rectangles
-	    .attr("height", function(d, i) {return (d * 0.002)})
-	    .attr("width","60")		//Width of the rectangles
-	    //.attr("x","25")			//Distance from the left side of the browser window
-	    .attr("x", function(d, i) {return i * 65 + 25})		//Add spacing between the rectangles, space = 60 pixels
-	    //.attr("y","50");		//Distance from the top of the browser window
-	    .attr("y", function(d, i) {return 600 - (d * 0.002)});
+        // render the value of the chart above the bar
+        ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontSize, 'normal', Chart.defaults.global.defaultFontFamily);
+        ctx.textAlign = 'center';
+        ctx.textBaseline = 'bottom';
 
-//Add text to the bars
-svg.selectAll("text")
-    .data(dataArray)
-    .enter().append("text")
-    .text(function(d) {return d})
-          .attr("class", "text")
-          .attr("x", function(d, i) {return (i * 65) + 31})
-          .attr("y", function(d, i) {return 620 - (d * 0.002)});
-
+        chartInstance.data.datasets.forEach(function (dataset) {
+            for (var i = 0; i < dataset.data.length; i++) {
+                var model = dataset._meta[Object.keys(dataset._meta)[0]].data[i]._model;
+                ctx.fillText(dataset.data[i], model.x, model.y - 2);
+            }
+        });
+  }
+});
