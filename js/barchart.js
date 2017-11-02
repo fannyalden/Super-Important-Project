9baab2496eed2d8ba8a72efc921b78f02a52ef14
Hanna Johansson <hanna93.hej@gmail.com>
2017-11-02 11:08:13 +0100

    changed color values

--- js/barchart.js
+++ js/barchart.js
@@ -20,21 +20,21 @@ new Chart(document.getElementById("bar-chart-grouped"), {
       datasets: [
         {
           label: "jTelefon",
-          backgroundColor: "rgba(255, 99, 132, 0.2)",
+          backgroundColor: "rgba(255, 99, 132, 0.6)",
           borderColor: "rgba(255, 99, 132, 1)",
           borderWidth: 1,
           data: [Cupertino.jTelefon, Norrköping.jTelefon, Frankfurt.jTelefon]
         }, 
         {
           label: "jPlatta",
-          backgroundColor: "rgba(54, 162, 235, 0.2)",
+          backgroundColor: "rgba(54, 162, 235, 0.6)",
           borderColor: "rgba(54, 162, 235, 1)",
           borderWidth: 1,
           data: [Cupertino.jPlatta, Norrköping.jPlatta, Frankfurt.jPlatta]
         },
         {
           label: "Päronklocka",
-          backgroundColor: "rgba(75, 192, 192, 0.2)",
+          backgroundColor: "rgba(75, 192, 192, 0.6)",
           borderColor: "rgba(75, 192, 192, 1)",
           borderWidth: 1,
           data: [Cupertino.Päronklocka, Norrköping.Päronklocka, Frankfurt.Päronklocka]
