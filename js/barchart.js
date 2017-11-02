512ec8bc5a7d33ccb670c3324f23919793593c98
Hanna Johansson <hanna93.hej@gmail.com>
2017-11-02 21:54:08 +0100

    changed back to one grouped bar chart

--- js/barchart.js
+++ js/barchart.js
@@ -2,81 +2,38 @@
     Grouped bar chart
 *************************************************************/
 
-new Chart(document.getElementById("bar-chart-grouped1"), {
+new Chart(document.getElementById("bar-chart-grouped"), {
     type: 'bar',
     data: {
-      //labels: ["Cupertino"],
+      labels: ["Cupertino", "Norrköping", "Frankfurt"],
       datasets: [
         {
           label: "jTelefon",
           backgroundColor: "rgba(255, 99, 132, 0.6)",
           borderColor: "rgba(255, 99, 132, 1)",
           borderWidth: 1,
-          data: [Cupertino.jTelefon]
+          data: [Cupertino.jTelefon, Norrkoping.jTelefon, Frankfurt.jTelefon]
         }, 
         {
           label: "jPlatta",
           backgroundColor: "rgba(54, 162, 235, 0.6)",
           borderColor: "rgba(54, 162, 235, 1)",
           borderWidth: 1,
-          data: [Cupertino.jPlatta]
-        },
-        {
-          label: "Paronklocka",
-          backgroundColor: "rgba(75, 192, 192, 0.2)",
-          borderColor: "rgba(75, 192, 192, 1)",
-          borderWidth: 1,
-          data: [Cupertino.Paronklocka]
-        }
-      ]
-    },
-    options: {
-    	legend: { display: true },
-	    title: {
-	        display: false,
-	        text: 'Lagersaldo'
-      	},
-        scales: {
-            yAxes: [{
-                display: false
-            }],
-
-        }
-    }
-});
-
-new Chart(document.getElementById("bar-chart-grouped2"), {
-    type: 'bar',
-    data: {
-      //labels: ["Norrköping"],
-      datasets: [
-        {
-          label: "jTelefon",
-          backgroundColor: "rgba(255, 99, 132, 0.6)",
-          borderColor: "rgba(255, 99, 132, 1)",
-          borderWidth: 1,
-          data: [Norrkoping.jTelefon]
-        }, 
-        {
-          label: "jPlatta",
-          backgroundColor: "rgba(54, 162, 235, 0.6)",
-          borderColor: "rgba(54, 162, 235, 1)",
-          borderWidth: 1,
-          data: [Norrkoping.jPlatta]
+          data: [Cupertino.jPlatta, Norrkoping.jPlatta, Frankfurt.jPlatta]
         },
         {
           label: "Päronklocka",
           backgroundColor: "rgba(75, 192, 192, 0.6)",
           borderColor: "rgba(75, 192, 192, 1)",
           borderWidth: 1,
-          data: [Norrkoping.Paronklocka]
+          data: [Cupertino.Paronklocka, Norrkoping.Paronklocka, Frankfurt.Paronklocka]
         }
       ]
     },
     options: {
       legend: { display: true },
       title: {
-          display: false,
+          display: true,
           text: 'Lagersaldo'
         },
         scales: {
@@ -88,50 +45,6 @@ new Chart(document.getElementById("bar-chart-grouped2"), {
     }
 });
 
-new Chart(document.getElementById("bar-chart-grouped3"), {
-    type: 'bar',
-    data: {
-      //labels: ["Frankfurt"],
-      datasets: [
-        {
-          label: "jTelefon",
-          backgroundColor: "rgba(255, 99, 132, 0.6)",
-          borderColor: "rgba(255, 99, 132, 1)",
-          borderWidth: 1,
-          data: [Frankfurt.jTelefon]
-        }, 
-        {
-          label: "jPlatta",
-          backgroundColor: "rgba(54, 162, 235, 0.6)",
-          borderColor: "rgba(54, 162, 235, 1)",
-          borderWidth: 1,
-          data: [Frankfurt.jPlatta]
-        },
-        {
-          label: "Päronklocka",
-          backgroundColor: "rgba(75, 192, 192, 0.6)",
-          borderColor: "rgba(75, 192, 192, 1)",
-          borderWidth: 1,
-          data: [Frankfurt.Paronklocka]
-        }
-      ]
-    },
-    options: {
-      legend: { display: true },
-      title: {
-          display: false,
-          text: 'Lagersaldo'
-        },
-        scales: {
-            yAxes: [{
-                display: false
-            }],
-
-        }
-    }
-});
-
-//https://stackoverflow.com/questions/31631354/how-to-display-data-values-on-chart-js
 //Add plugin to display values of each bar
 Chart.pluginService.register({
     afterDraw: function(chartInstance) {
