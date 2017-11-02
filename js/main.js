6e40ae5185b11e05c939171bbf4186d304bf4f50
Hanna Johansson <hanna93.hej@gmail.com>
2017-11-02 10:15:40 +0100

    added constructors for each city

--- js/main.js
+++ js/main.js
@@ -3,3 +3,29 @@
     Date: 2017-11-01
 *************************************************************/
 
+//Constructor for the warehouse in Cupertino 
+function Cupertino() {
+	
+	this.lagerID = 1;
+	this.jTelefon = 170000;
+	this.jPlatta = 41500;
+	this.Päronklocka = 90000; 
+};
+
+//Constructor for the warehouse in Norrköping
+function Norrköping() {
+	
+	this.lagerID = 2;
+	this.jTelefon = 55000;
+	this.jPlatta = 104300;
+	this.Päronklocka = 38000; 
+};
+
+//Constructor for the warehouse in Frankfurt
+function Frankfurt() {
+	
+	this.lagerID = 3;
+	this.jTelefon = 101700;
+	this.jPlatta = 72400;
+	this.Päronklocka = 25000; 
+};
